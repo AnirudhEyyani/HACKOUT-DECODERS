178b37e1585bf3c02dcb4f90d8eeb7113ccc5bec
AnirudhEyyani <anirudheyyani@outlook.com>
2021-10-30 22:43:31 +0530

    buy sell

--- src/components/Exchanges.jsx
+++ src/components/Exchanges.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import millify from 'millify';
-import { Collapse, Row, Col, Typography, Avatar } from 'antd';
+import { Collapse, Row, Col, Typography, Avatar, Button } from 'antd';
 import HTMLReactParser from 'html-react-parser';
 
 import { useGetExchangesQuery } from '../services/cryptoApi';
@@ -19,9 +19,9 @@ const Exchanges = () => {
     <>
       <Row>
         <Col span={6}>Exchanges</Col>
-        <Col span={6}>24h Trade Volume</Col>
-        <Col span={6}>Markets</Col>
-        <Col span={6}>Change</Col>
+        <Col span={5}>24h Trade Volume</Col>
+        <Col span={4}>Markets</Col>
+        <Col span={4}>Change</Col>
       </Row>
       <Row>
         {exchangesList.map((exchange) => (
@@ -37,9 +37,11 @@ const Exchanges = () => {
                       <Avatar className="exchange-image" src={exchange.iconUrl} />
                       <Text><strong>{exchange.name}</strong></Text>
                     </Col>
-                    <Col span={6}>${millify(exchange.volume)}</Col>
-                    <Col span={6}>{millify(exchange.numberOfMarkets)}</Col>
-                    <Col span={6}>{millify(exchange.marketShare)}%</Col>
+                    <Col span={5}>${millify(exchange.volume)}</Col>
+                    <Col span={5}>{millify(exchange.numberOfMarkets)}</Col>
+                    <Col span={5}>{millify(exchange.marketShare)}%</Col>
+                    <Col span={2}><Button type="primary">Buy</Button></Col>
+                    <Col span={1}><Button>Sell</Button></Col>
                   </Row>
                   )}
               >
